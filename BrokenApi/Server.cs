e871f1470b4353847b90c1aed561e886c4842e02
Matt Painter <matthewpainter@co-operativebank.co.nz>
2021-10-14 16:47:56 +1300

    Add repro code

---
+++ BrokenApi/Server.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace BrokenApi
+{
+    public class Server
+    {
+        public static async Task Main(string[] args) => await Initialise(args);
+
+        public static async Task<IWebHost> Initialise(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .Build();
+
+            var hostBuilder = WebHost.CreateDefaultBuilder(args)
+                .UseConfiguration(configuration)
+                .UseStartup<Startup>();
+
+            var webHost = hostBuilder.Build();
+            await webHost.RunAsync();
+
+            return webHost;
+        }
+    }
+}
---
+++ BrokenApi/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BrokenApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            return services.BuildServiceProvider();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app
+                .UseDeveloperExceptionPage()
+                .UseMvc();
+        }
+    }
+}
